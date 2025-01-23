bc0e4884f853804538b3b9315a287b0035486e6c
VOSolyanik <vitaly.solyanik@levelupers.com>
2025-01-23 22:08:38 +0000

    Deploying to gh-pages from @ VOSolyanik/project-impulse@15dc21b151fe88f6d2e104d277e905f78a99331a 🚀

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */console.log("This is home page");
+import"./assets/main-CmsXPnol.js";/* empty css                      */console.log("This is home page");
 //# sourceMappingURL=index.js.map
