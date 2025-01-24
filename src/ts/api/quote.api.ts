6d4846ee74b255631a527a6d93fb76e3f73372a2
yuratouch <yu.touch@icloud.com>
2025-01-24 15:32:11 +0200

    JP-8. Clean up fix

--- src/ts/api/quote.api.ts
+++ src/ts/api/quote.api.ts
@@ -1,4 +1,3 @@
-
 export const fetchQuote = async (): Promise<{ author: string; quote: string }> => {
     const response = await fetch('https://your-energy.b.goit.study/api/quote');
     if (!response.ok) {
