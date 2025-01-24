d9f061b6714a865e2752b87819354a15445ef515
yuratouch <yu.touch@icloud.com>
2025-01-24 15:31:39 +0200

    JP-8. Clean up fixes

--- src/ts/api/quote.api.ts
+++ src/ts/api/quote.api.ts
@@ -1,6 +1,4 @@
 
-// first draft to present functionality
-// TODO: refactor with Axios and the approach suggested by team leaed
 export const fetchQuote = async (): Promise<{ author: string; quote: string }> => {
     const response = await fetch('https://your-energy.b.goit.study/api/quote');
     if (!response.ok) {
@@ -8,15 +6,3 @@ export const fetchQuote = async (): Promise<{ author: string; quote: string }> =
     }
     return response.json();
 };
-
-
-// --- saved as a draft after code conflict resolution ---
-
-// import { yourEnergyApi } from './base';
-// import { QuoteOfDay } from '../types/quote';
-
-// export const getQuoteOfTheDay = async (): Promise<QuoteOfDay> => {
-//   const response = await yourEnergyApi.get<QuoteOfDay>('/quote');
-//   return response.data;
-// };
-
