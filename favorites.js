d6425cbbd50c9767ca75035cf7a09f1aa47ab7c7
VOSolyanik <vitaly.solyanik@levelupers.com>
2025-01-26 10:51:51 +0000

    Deploying to gh-pages from @ VOSolyanik/project-impulse@7bd8d50c60b4f666e966beeff8bd17f71042e83c 🚀

--- favorites.js
+++ favorites.js
@@ -1,2 +1,2 @@
-import{P as o}from"./assets/main-DVPzL8Mt.js";/* empty css                      */const a=document.querySelector(".pagination-wrapper"),t=new o(a,5,12);t.onPageChange((e,n)=>{console.log("Make API request here",e,n)});
+import{P as n}from"./assets/main-Dr8wReif.js";/* empty css                      */import"./assets/vendor-4jich7g5.js";const a=document.querySelector(".pagination-wrapper"),t=new n(a,5,12);t.onPageChange((o,e)=>{console.log("Make API request here",o,e)});
 //# sourceMappingURL=favorites.js.map
