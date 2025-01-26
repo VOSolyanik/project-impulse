9c6392f58ed79fd7f1455fb2d8c99f1455941b60
VOSolyanik <v.o.solyanik@gmail.com>
2025-01-27 00:41:19 +0200

    Fix fonts 4

--- vite.config.js
+++ vite.config.js
@@ -15,6 +15,7 @@ export default defineConfig(({ command }) => {
     resolve: {
       alias: {
         "@": path.resolve(__dirname, "./src/ts"),
+        "assets": path.resolve(__dirname, "./src/assets"),
       },
     },
     build: {
